833eea250f67509960c34242268866cb9f3dafd1
Daniel Gorianz <aalbaafikaa@gmail.com>
2022-03-31 08:51:16 -0400

    lista de superheroes mostrados

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useParams, Navigate } from 'react-router-dom'
+import { getHeroById } from '../../helpers/getHeroById'
 
 export const HeroScreen = () => {
+
+  const { id } = useParams()
+
+  const hero = getHeroById( id )
+
+  if (!hero) return <Navigate to={'/'}/> //! Navigate se encargar hacer la navegacion
+
   return (
     <div>
         <h1>Hero Screen</h1>
+        <p>
+          {hero.superhero}
+        </p>
     </div>
   )
 }
