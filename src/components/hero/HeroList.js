101820a1d5ef8d9541c88bad6c7cc321960fe200
Daniel Gorianz <aalbaafikaa@gmail.com>
2022-03-31 09:23:14 -0400

    ver mas y animaciones terminadas

--- src/components/hero/HeroList.js
+++ src/components/hero/HeroList.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import PropTypes from 'prop-types'
 import { getHeroesByPublisher } from '../../helpers/getHeroesByPublisher'
 import { HeroCard } from './HeroCard'
 
 export const HeroList = ({ publisher }) => {
 
-    const heroes = getHeroesByPublisher( publisher )
+    const heroes = useMemo(()=>getHeroesByPublisher( publisher ), [publisher]) 
 
     return (
-        <div className='row row-cols-1 row-cols-md-3 g-3'>
+        <div className='row row-cols-1 row-cols-md-3 g-3 animate__animated animate__fadeIn'>
             { heroes.map( hero => <HeroCard key={hero.id} hero = {hero}/> ) }
         </div>
     )
